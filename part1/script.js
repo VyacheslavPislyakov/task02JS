5c4a145428d5c8e375667d3194fb22cd6418f2bd
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2017-06-27 23:12:21 +0300

    add new func

--- part1/script.js
+++ part1/script.js
@@ -1,9 +1,9 @@
 //Class Vegetables
 function Vegetables(name, weight) {
-	var category;
+	// var category;
 	this.name = name;
 	this.weight = weight;
-};
+}
 Vegetables.prototype.getName = function() {
 	return this.name;
 };
@@ -12,14 +12,14 @@ Vegetables.prototype.getName = function() {
 function NightshadeVeg(name, weight) {
 	Vegetables.call(this, name, weight);
 	this.category = "Nightshade Vegetables";
-};
+}
 NightshadeVeg.prototype = new Vegetables();
 
 //Class Root Vegetables
 function RootVeg(name, weight) {
 	Vegetables.call(this, name, weight);
 	this.category = "Root Vegetables";
-};
+}
 RootVeg.prototype = new Vegetables();
 
 //Class Cucurbitaceae Vegetables
@@ -33,14 +33,14 @@ CucurbitaceaeVeg.prototype = new Vegetables();
 function Tomato(name, weight, calories) {
 	NightshadeVeg.call(this, name, weight);
 	this.calories = calories * (100 / weight);
-};
+}
 Tomato.prototype = new NightshadeVeg();
 
 //Class eggplant
 function Eggplant(name, weight, calories) {
 	NightshadeVeg.call(this, name, weight);
 	this.calories = calories * (100 / weight);
-};
+}
 Eggplant.prototype = new NightshadeVeg();
 
 //Class Carrot
@@ -74,20 +74,19 @@ Cucumber.prototype = new CucurbitaceaeVeg();
 
 //Class create Salad
 function CreateSalad() {
-	var nameSalat = "Summer salad";
 	this.listProducts = [];
 	this.totalCalories = 0;
-};
-CreateSalad.prototype.addProduct = function (product) {
+}
+CreateSalad.prototype.addProduct = function(product) {
 	this.listProducts.push(product);
 };
-CreateSalad.prototype.getListProducts = function () {
+CreateSalad.prototype.getListProducts = function() {
 	return this.listProducts;
 };
-CreateSalad.prototype.getRecipe = function () {
+CreateSalad.prototype.getRecipe = function() {
 	for (var i = 0; i < cs.getListProducts().length; i++) {
 		if (cs.getListProducts()[i].hasOwnProperty("calories")) {
-			var insertLi = document.createElement('li');
+			var insertLi = document.createElement("li");
 			insertLi.innerHTML = cs.getListProducts()[i].name + " " + cs.getListProducts()[i].weight + " г.";
 			recipeList.appendChild(insertLi);
 		}
@@ -102,23 +101,35 @@ CreateSalad.prototype.getTotalCalories = function() {
 	}
 	var pTag = document.createElement("div");
 	pTag.innerHTML = "Общая калорийность салата - " + Math.round(this.totalCalories);
-	var myP = document.getElementById("recipe").appendChild(pTag);
+	document.getElementById("recipe").appendChild(pTag);
 	return Math.round(this.totalCalories);
+};
 
+function compareCalories(personA, personB) {
+	return personA.calories - personB.calories;
 }
 
 var cs = new CreateSalad();
 cs.addProduct(new Cucumber("Огурец", 200, 35));
 cs.addProduct(new Carrot("Морковка", 300, 335));
 cs.addProduct(new Pumpkin("Тыква", 100, 50));
-cs.getRecipe();
-cs.getTotalCalories();
+console.log(cs.getListProducts());
 
+var sortProducts = cs.getListProducts();
+sortProducts.sort(compareCalories);
+console.log(sortProducts);
+
+// var buttonCalories = document.getElementById("buttonCalories");
+// buttonCalories.addEventListener("click", cs.getTotalCalories());
 // console.log(cs.getTotalCalories());
 
-// var insertLi = document.createElement('li');
-// insertLi.innerHTML = "1";
-// recipeList.appendChild(insertLi);
+
+//Class Sorted by calories
+
+
+
+
+
 
 
 ////убрать методы с prototype
