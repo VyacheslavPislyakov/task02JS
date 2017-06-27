3746f60297f0473c888c9f31e9edda01256d3c86
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2017-06-27 14:27:44 +0300

    inheritance

--- part1/script.js
+++ part1/script.js
@@ -1,41 +1,99 @@
 //Class Vegetables
-function Vegetables(name, color, weight) {
+function Vegetables(name, weight) {
+	var category;
 	this.name = name;
-	this.color = color;
 	this.weight = weight;
 };
 Vegetables.prototype.getName = function () {
 	return this.name;
 };
 
-//Class Homemade Vegetables
-function HomeVegetables(name, color, weight) {
-	Vegetables.call(this, arguments);
-	this.category = "Homemade Vegetables";
+Vegetables.prototype.setCategory = function (category) {
+	this.category = category;
 };
 
-//Class Exotic Vegetables
-function ExoticVegetables(name, color, weight) {
+//Class Nightshade Vegetables
+function NightshadeVeg(name, weight) {
 	Vegetables.call(this, arguments);
-	this.category = "Exotic Vegetables"
+	this.category = "Nightshade Vegetables";
 };
 
-//Class Potato
-function Potato() {
-	HomeVegetables.call(this, arguments);
-	this.name = "Potato";
-	this.weight = 300;
-	this.color = "red";
+//Class Root Vegetables
+function RootVeg(name, weight) {
+	Vegetables.call(this, arguments);
+	this.category = "Root Vegetables";
+};
+
+//Class Cucurbitaceae Vegetables
+function CucurbitaceaeVeg(name, weight) {
+	Vegetables.call(this, arguments);
+	this.category = "Cucurbitaceae Vegetables";
+}
+
+
+//Class Tomato
+function Tomato() {
+	NightshadeVeg.call(this, arguments);
+	this.name = "Tomato";
+	this.weight = 100;
+	this.calories = 75;
+};
+Tomato.prototype = new Vegetables();
+
+//Class eggplant
+function Eggplant() {
+	NightshadeVeg.call(this, arguments);
+	this.name = "Eggplant";
+	this.weight = 100;
 	this.calories = 375;
 };
-Potato.prototype = new Vegetables();
+Eggplant.prototype = new Vegetables();
+
+//Class Carrot
+function Carrot(){
+	RootVeg.call(this, arguments);
+	this.name = "Carrot";
+	this.weight = 100;
+	this.calories = 130;
+}
+Carrot.prototype = new Vegetables();
+
+//Class Beet
+function Beet(){
+	RootVeg.call(this, arguments);
+	this.name = "Beet";
+	this.weight = 100;
+	this.calories = 230;
+}
+Beet.prototype = new Vegetables();
+
+//Class Pumpkin
+function Pumpkin() {
+	CucurbitaceaeVeg.call(this, arguments);
+	this.name = "Pumpkin";
+	this.weight = 100;
+	this.calories = 330;
+}
+
+//Class Cucumber
+function Cucumber() {
+	CucurbitaceaeVeg.call(this, arguments);
+	this.name = "Cucumber";
+	this.weight = 100;
+	this.calories = 30;
+}
 
 
 
 
 
-var potato = new Potato();
-console.log(potato.getName());
+var tomato = new Tomato();
+console.log(tomato);
+console.log(tomato.getName());
+
+var eggplant = new Eggplant();
+console.log(eggplant);
+console.log(eggplant.getName());
 
 
 // var veg1 = new HomeVegetables("Potato", "red", 250);
