888cc63667528e55f4bf62beb20af8fc44c3ae0b
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2017-06-30 12:51:18 +0300

    repair script

--- part2/script.js
+++ part2/script.js
@@ -1,13 +1,34 @@
 var Car = {
 	name: "car",
 	type: "coupe",
+	model: "model",
 	wheels: 4,
 	chiping: {
 		status: false,
+	},
+	sound: {
+		sub: "standart",
+		columns: "standart",
 	}
 };
 
-var SpeenCar = {};
+var SpeenCar = {
+	name: "Mercedes",
+	type: "coupe",
+	model: "E350",
+	maxSpeed: 350,
+	chiping: {
+		status: true,
+		company: "AMG"
+	},
+	sound: {
+		sub: "BOSS",
+		columns: "Pioneer",
+		headDevice: "Pioneer",
+	}
+
+
+};
 
 function extendDeep(parent, child) {
 	var i,
@@ -15,11 +36,16 @@ function extendDeep(parent, child) {
 		astr = "[object Array]";
 
 	child = child || {};
+
 	for (i in parent) {
 		if (parent.hasOwnProperty(i)){
 			if (typeof parent[i] === "object") {
-				child[i] = (toStr.call(parent[i]) === astr) ? [] : {};
+				if (!child[i]){
+					child[i] = (toStr.call(parent[i]) === astr) ? [] : {};
+				}
 				extendDeep(parent[i], child[i]);
+			} else if (child.hasOwnProperty(i)) {
+				continue;
 			} else {
 				child[i] = parent[i];
 			}
@@ -30,18 +56,13 @@ function extendDeep(parent, child) {
 
 
 var fastCar = extendDeep(Car, SpeenCar);
-fastCar.name = "Mercedes";
-fastCar.model = "E350";
-fastCar.maxSpeed = 350;
-fastCar.chiping.status = true;
-fastCar.chiping.company = "AMG";
 
 var elem = document.createElement("div");
-elem.innerHTML = "Parent property: Name: " + Car.name + " Type: " + Car.type + " Wheels: " + Car.wheels + " Chiping: " + Car.chiping.status;
+elem.innerHTML = "Parent property: Name: " + Car.name +  "; Model: " + Car.model +  " Type: " + Car.type + "; Wheels: " + Car.wheels + "; Chiping: " + Car.chiping.status + "; Sound Sub: " + Car.sound.sub + "; Sound Columns:  " + Car.sound.columns;
 // console.log(fastCar);
 document.body.appendChild(elem);
 
 var elem1 = document.createElement("div");
-elem1.innerHTML = "Child property Name: " + fastCar.name + " Model: " + fastCar.model + " Type: " + fastCar.type +" Max.Speed: " + fastCar.maxSpeed + " Wheels: " + fastCar.wheels + " Chiping: " + fastCar.chiping.status + " company " + fastCar.chiping.company;
+elem1.innerHTML = "Child property Name: " + fastCar.name + " Model: " + fastCar.model + " Type: " + fastCar.type + " Wheels: " + fastCar.wheels + " Chiping: " + fastCar.chiping.status + "; Sound Sub: " + fastCar.sound.sub + "; Sound Columns:  " + fastCar.sound.columns + "; Sound head device: " + fastCar.sound.headDevice + " Max.Speed: " + fastCar.maxSpeed +  " company " + fastCar.chiping.company;
 console.log(fastCar);
 document.body.appendChild(elem1);
